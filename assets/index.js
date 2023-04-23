c722784b33175556ea820dbe91bdd3779e39c1d1
Max Kuh <maxes2805582@gmail.com>
2023-04-23 23:51:39 +0300

    Add unshift,shift

--- assets/index.js
+++ assets/index.js
@@ -1,61 +1,49 @@
 'use strict';
-// Перше завдання
-const testArray = [1, 2, 3];
-testArray.push(6, 5, 4);
-testArray.unshift(-3, -2, -1);
-// Друге завдання
-const testArray2 = [100, -273.15, 0, 1, 2, 3, 4, 5, 6, 9, 10];
-testArray2.splice(0, 2);
-testArray2.splice(-2, 2);
-// Третє завдання
-const testArray3 = [1, 2, 3, 4, 5];
-testArray3.splice(1, 2);
-// Четверте завдання
-const testArray4 = [1, 2, 3, 4, 5];
-const returnArray = testArray4.splice(1, 3);
-// П'яте завдання
-const testArray5 = [1, 2, 3, 4, 5];
-testArray5.splice(2, 0, 'a', 'b', 'c');
-// П'яте завдання
-const testArray6 = [1, 2, 3, 4, 5];
-/*testArray6.splice(1,0,'a','b');
-testArray6.splice(-1,0,'c');
-testArray6.splice(-1,1,'5','e'); */
-// Або видалити всі і перезапизати , так напевно коротше буде
-testArray6.splice(1, 5, 2, 'a', 'b', 3, 4, 'c', 5, 'e');
-
-// ================ PART 2 ==================
-// Перше завдання
-const presentArray = [10, 20, 30, 40, 50, 60];
-const halfPrsArray = presentArray.slice(0, presentArray.length / 2);
-const partPrsArray = presentArray.slice(-2, presentArray.length);
-// Друге завдання
-const ottArray = [1, 2, 3];
-const ffsArray = [4, 5, 6];
-const concatedArray = ottArray.concat(ffsArray);
-// Третє завдання
-const nesArray = [9, 8, 7];
-nesArray.reverse();
-// Четверте завдання
-const mixedArray = [5, 8, 44, 32, 14, 1, 3];
-function sortingfunction(a, b) {
-  if (a > b) {
-    return -1;
-  }
-  return 1;
+function MyArray() {
+  this.length = 0;
 }
-mixedArray.sort(sortingfunction);
-// П'яте завдання
-const forEachArray = [2, 5, 8, 7, 9];
-forEachArray.forEach(function (a) {
-  console.log(a);
-});
-forEachArray.forEach(function (a, b) {
-  console.log(`Елемент ${a} має індекс ${b}`);
-});
-// Шосте завдання
 
-const mapOldArray = [10, 9, 8, 7, 6, 5];
-const mapNewArray = mapOldArray.map(function (a, b) {
-  return a + b;
-});
+function MyArrayProto() {
+  this.push = function (newElem) {
+    // записати елемент
+    this[this.length] = newElem;
+    // оновили довжину масиву
+    this.length++;
+    return this.length;
+  };
+
+  this.pop = function () {
+    if (this.length > 0) {
+      const lastItem = this[this.length - 1];
+      delete this[--this.length];
+
+      return lastItem;
+    }
+
+    return undefined;
+  };
+  this.unshift = function (newElem) {
+    for (let i = this.length; i > 0; i--) {
+      this[i] = this[i - 1];
+    }
+    this.length++;
+    this[0] = newElem;
+    return this.length;
+  };
+  this.shift = function () {
+    if (this.length > 0) {
+      const firstItem = this[0];
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1];
+      }
+      delete this[--this.length];
+      return firstItem;
+    }
+    return undefined;
+  };
+}
+MyArray.prototype = new MyArrayProto();
+
+const myArr1 = new MyArray();
+
+const arr = [];
