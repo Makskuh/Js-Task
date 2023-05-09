1420ee416c4a7934dcca83343848fe9225d54897
Max Kuh <maxes2805582@gmail.com>
2023-05-09 20:48:04 +0300

    Add 5s sltn

--- assets/index.js
+++ assets/index.js
@@ -32,21 +32,58 @@ const discountDiamond = {
   caschBack: 2,
   credit: 10000,
 };
-const lvlCard = new Map([
-  ['basic', discountBasic],
-  ['pro', discountPro],
-  ['diamond', discountDiamond]
-]);
 const bank = {
   name: 'Mono',
   contact: '+380440001002',
   adress: 'Kyiv,str.Vel Vasulkivska,2',
-  CardLvl(client) {
-    return lvlCard.get(client.lvlCard);
-  },
+  CardLvl: new Map([
+    ['basic', discountBasic],
+    ['pro', discountPro],
+    ['diamond', discountDiamond],
+  ]),
 };
 function fullInfo(client) {
-  return `${client.name} ${client.lastName} has id:${client.id} , card number ${client.numbCard} and still has  discount: ${bank.CardLvl(client).discount} % , cashBack: ${bank.CardLvl(client).caschBack} %, credit limit: ${bank.CardLvl(client).credit} usdt`
+  return `${client.name} ${client.lastName} has id:${client.id} , card number ${
+    client.numbCard
+  } and still has  discount: ${
+    bank.CardLvl.get(client.lvlCard).discount
+  } % , cashBack: ${
+    bank.CardLvl.get(client.lvlCard).caschBack
+  } %, credit limit: ${bank.CardLvl.get(client.lvlCard).credit} usdt`;
 }
 
 // 3.0 завдання
+const array20 = [0, 0, 0];
+for (let i = 0; i < 10; i++) {
+  array20.unshift(Math.round(Math.random() * -100));
+  array20.push(Math.round(Math.random() * 100));
+}
+//3.1
+// array20.forEach((value, i) => (i % 2 === 0 ? console.log(i) : undefined));
+//3.2
+// array20.forEach((value) =>
+//   !(value % 2 === 0) ? console.log(value) : undefined
+// );
+//3.3
+// array20.forEach((value, i) => (value < 0 ? console.log(i) : undefined));
+//3.4
+// console.log(array20.filter((value) => value === 0).length)
+
+
+//4 Завдання
+class Book {
+  constructor(author,name,yearOfPubl,publication) {
+    this.author = author;
+    this.name = name;
+    this.yearOfPubl = yearOfPubl;
+    this.publication = publication
+  }
+}
+class EBook extends Book {
+  constructor(author,name,yearOfPubl,publication,format,eNumb) {
+    super(author,name,yearOfPubl,publication)
+    this.format = format;
+    this.eNumb = eNumb;
+  }
+}
+//5 Завдання 
